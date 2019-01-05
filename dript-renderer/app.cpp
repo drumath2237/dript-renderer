4af8dae4baffad1bcd6b8b503e86814282d1df12
drumath <2237k.drumath@gmail.com>
2019-01-05 13:51:15 +0900

    交差判定の検証が完了

--- dript-renderer/app.cpp
+++ dript-renderer/app.cpp
@@ -10,15 +10,39 @@ using namespace std;
 
 int main()
 {
+	int width = 800;
+	int height = 800;
+	PPM ppm = PPM(width, height);
+
 	Triangle tri;
-	tri.v0 = Vec();
+	tri.v0 = Vec(0, 0, 1);
 	tri.v1 = Vec(1, 1, 0);
 	tri.v2 = Vec(1, -1, 0);
 
-	Ray ray;
-	ray.o = Vec(0.5, 0, 5);
-	ray.d = Vec(0, 0, -1);
+	vector<Vec> Color = vector<Vec>();
 
-	double t = intersect(tri, ray, 0.1, 100);
-	cout << t << endl;
+	Ray ray;
+
+	for (int i = 0; i < width*height; i++)
+	{
+		int w = i % height;
+		int h = i / height;
+
+		double x = (double)w / 400. - 1.;
+		double y = (double)h / 400. - 1.;
+
+		ray.o = Vec(x, y, 5);
+		ray.d = Vec(0, 0, -1);
+
+		double t = intersect(tri, ray, 0.001, 100);
+		if (t >= 0) {
+			Color.push_back(Vec(255, 255, 0));
+		}
+		else {
+			Color.push_back(Vec());
+		}
+	}
+
+	ppm_out(ppm, Color);
+
 }
--- dript-renderer/triangle.h
+++ dript-renderer/triangle.h
@@ -7,6 +7,10 @@ struct Triangle
 {
 	Vec v0, v1, v2;
 	Vec normal;
+
+	Triangle() {};
+	Triangle(Vec v0, Vec v1, Vec v2) : v0(v0), v1(v1), v2(v2) {};
+	Triangle(Vec v0, Vec v1, Vec v2, Vec normal_) : v0(v0), v1(v1), v2(v2), normal(normal_) {};
 };
 
 double intersect(Triangle polygon, Ray ray, double tmin, double tmax)
