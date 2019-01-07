f1ad7ce1bd2001f36bde5ea9f82159ac597e6900
drumath <2237k.drumath@gmail.com>
2019-01-08 01:46:51 +0900

    交差判定にsceneを導入した

--- dript-renderer/app.cpp
+++ dript-renderer/app.cpp
@@ -6,6 +6,7 @@
 #include "triangle.h"
 #include "vec.h"
 #include "intersect.h"
+#include "scene.h"
 
 using namespace std;
 
@@ -15,10 +16,18 @@ int main()
 	int height = 800;
 	PPM ppm = PPM(width, height);
 
-	Triangle tri;
-	tri.v0 = Vec(0, 0, 1);
-	tri.v1 = Vec(1, 1, 0);
-	tri.v2 = Vec(1, -1, 0);
+	Triangle tri1, tri2;
+	tri1.v0 = Vec();
+	tri1.v1 = Vec(0, 0.5, 0);
+	tri1.v2 = Vec(0.5, 0, 0);
+
+	tri2.v0 = Vec(-0.47, 0.61, 0);
+	tri2.v1 = Vec(-.75, -.19, 0);
+	tri2.v2 = Vec(-.33, 0.24, 0);
+
+	Scene scene;
+	scene.polygons.push_back(tri1);
+	scene.polygons.push_back(tri2);
 
 	vector<Vec> Color = vector<Vec>();
 
@@ -35,10 +44,8 @@ int main()
 		ray.o = Vec(x, y, 5);
 		ray.d = Vec(0, 0, -1);
 
-		//double t = intersect(tri, ray, 0.001, 100);
-		Hit hit = isRayIntersectPolygon(tri, ray, 0.001, 100);
-		double t = hit.t;
-		if (t >= 0) {
+		Hit hit = scene.intersect(ray, 0.001, 100);
+		if (hit.isHit) {
 			Color.push_back(Vec(255, 0, 255));
 		}
 		else {
